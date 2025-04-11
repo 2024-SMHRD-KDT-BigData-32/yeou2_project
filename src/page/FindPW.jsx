d89ca36fdc510e5492a88a499e64fda66a0f7845
Cho Seok Bin <tjrqls1417@naver.com>
2025-04-11 10:02:35 +0900

    석빈_04/11_10:02_findPW 디자인 수정

--- src/page/FindPW.jsx
+++ src/page/FindPW.jsx
@@ -5,10 +5,10 @@ import useMove from "../components/Fun.jsx"
 
 const FindPW = () => {
 
-    
+
 
     const {
-        
+
         moveSignUp,
         moveLogin,
         moveFindId,
@@ -17,33 +17,40 @@ const FindPW = () => {
 
 
 
-    
+
     return (
 
         <div id="findPW">
             <div className="mainLabel">비밀번호찾기</div>
 
             <div className="idLabel">ID</div>
-            <input className="idInput" type='text'></input>
+            <div className='group'>
+                <input className="idInput" type='text'></input>
+            </div>
 
             <div className="nameLabel">이름</div>
-            <input className="nameInput" type='text'></input>
+            <div className='group'>
+                <input className="nameInput" type='text'></input>
+            </div>
 
             <div className="emailLabel">이메일</div>
-            <input className="emailInput" type='text'></input>
-            <button className="emailBtn">전송</button>
+            <div className='group'>
+                <input className="emailInput" type='text'></input>
+                <button className="emailBtn">전송</button>
+            </div>
 
             <div className="authLabel">인증번호</div>
-            <input className="authInput" type='text'></input>
-            <button className="authBtn">확인</button>
-            
-            <button className="findPWBtn"onClick={()=>moveFindPw()}>비밀번호 찾기</button>
-            
-            <button className="signUpBtn"onClick={()=>moveSignUp()}>회원가입</button>
-            <button className="loginBtntext"onClick={()=>moveLogin()}>로그인</button>
-            <button className="findIDBtn" onClick={()=>moveFindId()}>아이디 찾기</button>
+            <div className='group'>
+                <input className="authInput" type='text'></input>
+                <button className="authBtn">확인</button>
+            </div>
+            <button className="findPWBtn" onClick={() => moveFindPw()}>비밀번호 찾기</button>
 
-            
+            <button className="signUpBtn" onClick={() => moveSignUp()}>회원가입</button>
+            <button className="loginBtntext" onClick={() => moveLogin()}>로그인</button>
+            <button className="findIDBtn" onClick={() => moveFindId()}>아이디 찾기</button>
+
+
         </div>
 
 
