d4f469a476c8f816af2cd00604212382ae5b7d13
Cho Seok Bin <tjrqls1417@naver.com>
2025-04-11 09:51:44 +0900

    석빈 04/11_09:51_find id 수정

--- src/page/FindID.jsx
+++ src/page/FindID.jsx
@@ -6,10 +6,10 @@ import useMove from "../components/Fun.jsx"
 
 const FindID = () => {
 
-    
+
 
     const {
-        
+
         moveSignUp,
         moveLogin,
         moveFindId,
@@ -18,28 +18,32 @@ const FindID = () => {
     } = useMove(); // ✅ Hook을 호출하여 네비게이션 함수 가져오기
 
 
-    
+
     return (
         <div id="findID">
             <div className="mainLabel">아이디찾기</div>
-            
+
             <div className="nameLabel">이름</div>
             <input className="nameInput" type="text"></input>
 
             <div className="emailLabel">이메일</div>
-            <input className="emailInput" type="text"></input>
-            <button className="emailBtn" >전송</button>
-            <br />
-            
+            <div className="emailGroup">
+                <input className="emailInput" type="text"></input>
+                <button className="emailBtn" >전송</button>
+            </div>
+
             <div className="authLabel">인증번호</div>
-            <input className="authInput" type="text"></input>
-            <button className="authBtn">인증번호확인</button>
-            <button className="loginBtn" onClick={()=>moveLogin()}>로그인</button>
-            
-            <button className="signUpBtn" onClick={()=>moveSignUp()}>회원가입</button>
-            
-            <button className="findPWBtn" onClick={()=>moveFindPw()}>비밀번호 찾기</button>
-            
+            <div className="authGroup">
+                <input className="authInput" type="text"></input>
+                <button className="authBtn">인증번호확인</button>
+            </div>
+            <div className="linkButtonGroup">
+                <a className="loginBtn" onClick={() => moveLogin()}>로그인</a>
+
+                <a className="signUpBtn" onClick={() => moveSignUp()}>회원가입</a>
+
+                <a className="findPWBtn" onClick={() => moveFindPw()}>비밀번호 찾기</a>
+            </div>
         </div>
 
     )
